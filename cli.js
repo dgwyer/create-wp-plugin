1384636ac3216239dec2e222e5e51d6b0197e8a9
dgwyer <d.v.gwyer@gmail.com>
2018-12-04 16:54:21 +0000

    updated cli.js

--- cli.js
+++ cli.js
@@ -1,82 +1,130 @@
 #!/usr/bin/env node
 const inquirer = require("inquirer");
-const argv = require("yargs").argv;
+//const argv = require("yargs").argv;
 const values = require("./input-data");
 const chalk = require("chalk");
 const chalkPipe = require("chalk-pipe");
+var figlet = require("figlet");
+const isSemver = require("is-semver");
+const logSymbols = require("log-symbols");
 
-console.log("Welcome to create-wp-plugin\n");
-console.log("[v1.0.1] Nothing to see yet, but coming soon...\n");
-return;
+console.clear();
 
-if (argv.ships > 3 && argv.distance < 53.5) {
-  console.log("Plunder more riffiwobbles!");
-} else {
-  console.log("Retreat from the xupptumblers!");
-}
+console.log(
+  figlet.textSync("create-wp-plugin", {
+    //font: "Ghost",
+    horizontalLayout: "default",
+    verticalLayout: "default"
+  })
+);
+console.log("\nCreated by David Gwyer");
+console.log("Twitter: https://twitter.com/dgwyer");
+console.log("GitHub: https://github.com/dgwyer/wp-block\n");
+console.log("-----\n");
+
+// if (argv.ships > 3 && argv.distance < 53.5) {
+//   console.log("Plunder more riffiwobbles!");
+// } else {
+//   console.log("Retreat from the xupptumblers!");
+
+const plugin_defaults = {
+  name: "New WP Plugin",
+  author: "David Gwyer",
+  version: "0.0.1"
+};
 
 const questions = [
   {
-    type: "list",
-    name: "coffeType",
-    message: "Choose coffee type",
-    choices: values.typesPlain
-  },
-  {
-    type: "list",
-    name: "sugarLevel",
-    message: "Choose your sugar level",
-    choices: values.sugarPlain
-  },
-  {
-    type: "confirm",
-    name: "decaf",
-    message: "Do you prefer your coffee to be decaf?",
-    default: false
-  },
-  {
-    type: "confirm",
-    name: "cold",
-    message: "Do you prefer your coffee to be cold?",
-    default: false
-  },
-  {
-    type: "list",
-    name: "servedIn",
-    message: "How do you prefer your coffee to be served in",
-    choices: values.servedIn
-  },
-  {
-    type: "confirm",
-    name: "stirrer",
-    message: "Do you prefer your coffee with a stirrer?",
-    default: true
-  },
-  {
     type: "input",
-    name: "first_name",
-    message: "What's your first name"
-  },
-  {
-    type: "input",
-    name: "last_name",
-    message: "What's your last name",
+    name: "plugin_name",
+    message: "Plugin Name:",
     default: function() {
-      return "Doe";
+      return plugin_defaults.name;
+    },
+    filter: function(input) {
+      return input.trim();
+    },
+    validate: function(input) {
+      if (input.trim()) {
+        console.log(logSymbols.success);
+        return true;
+      } else {
+        console.log(chalk.red("(required)"));
+        return false;
+      }
     }
   },
   {
     type: "input",
-    name: "fav_color",
-    message: "What's your favorite color",
-    transformer: function(color, answers, flags) {
-      const text = chalkPipe(color)(color);
-      if (flags.isFinal) {
-        return text;
+    name: "plugin_author",
+    message: "Plugin Author:",
+    filter: function(input) {
+      return input.trim();
+    }
+  },
+  {
+    type: "input",
+    name: "plugin_version",
+    message: "Plugin Version:",
+    default: function() {
+      return "0.0.1";
+    },
+    filter: function(input) {
+      return input.trim();
+    },
+    validate: function(input) {
+      if (isSemver(input)) {
+        console.log(chalk.green("(required)"));
+        return true;
+      } else {
+        console.log(chalk.red("(required)"));
+        return false;
       }
-      return text;
+    }
+  },
+  {
+    type: "input",
+    name: "plugin_text_domain",
+    message: "Text Domain:",
+    default: function() {
+      return "wordpress";
+    },
+    filter: function(input) {
+      return input.trim();
     }
   }
+  // {
+  //   type: "list",
+  //   name: "servedIn",
+  //   message: "How do you prefer your coffee to be served in",
+  //   choices: values.servedIn
+  // },
+  // {
+  //   type: "confirm",
+  //   name: "stirrer",
+  //   message: "Do you prefer your coffee with a stirrer?",
+  //   default: true
+  // },
+  // {
+  //   type: "input",
+  //   name: "last_name",
+  //   message: "What's your last name",
+  //   default: function() {
+  //     return "Doe";
+  //   }
+  // },
+  // {
+  //   type: "input",
+  //   name: "fav_color",
+  //   message: "What's your favorite color",
+  //   transformer: function(color, answers, flags) {
+  //     const text = chalkPipe(color)(color);
+  //     if (flags.isFinal) {
+  //       return text;
+  //     }
+  //     return text;
+  //   }
+  // }
 ];
 
 inquirer.prompt(questions).then(function(answers) {
@@ -84,3 +132,9 @@ inquirer.prompt(questions).then(function(answers) {
 
   // dir as option to specify folder name that's different from plugin name >>> --dir="wpgoplugins"
 });
+
+// To Do
+//
+// 1. Check if plugin folder already exists? Or warn when plugin is running and exit.
+// 2. Whitelist characters for certain fields such as plugin name (look up allowed characters).
+// 3. Implement global config object but where to store this when calling npx?
